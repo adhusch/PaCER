979fb413450eda4035c41c1afa60b20d3c567e3e
laurentheirendt <laurent.heirendt@uni.lu>
2018-10-11 17:10:44 +0200

    changes to script to launch matlab

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$ARCH" == "Linux" ]; then
-    /mnt/prince-data/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash -r "fprintf('Hello Jenkins.\n'); quit();"
+    /mnt/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash -r "fprintf('Hello ARTENOLIS.\n'); quit();"
 fi
 
 CODE=$?
