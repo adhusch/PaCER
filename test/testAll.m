ecb038bcbf7daf45133662b58cf58831e85254b4
Loic Marx <loic.marx@gmail.com>
2019-03-04 15:14:14 +0100

    implementation of testAll

--- test/testAll.m
+++ test/testAll.m
@@ -1,1 +1,12 @@
--r "fprintf('Hello ARTENOLIS.\n'); quit();"
+fprintf('Hello ARTENOLIS.\n'); 
+% if the location of pacer is not yet known
+if isempty(which('SETUP_PACER.m'))
+   % move back to the root of the repository
+   cd([fileparts(which('testAll.m')) filesep '..'])
+
+   % assign the path
+   CBTDIR = pwd;
+else
+   CBTDIR = fileparts(which('SETUP_PACER.m'));
+   cd(CBTDIR);
+end 
