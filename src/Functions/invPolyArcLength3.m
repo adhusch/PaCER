2c0908acc8fafa82070ef560e8be369b8fea226d
Daniel Duarte Tojal <daniel.duarte.001@student.uni.lu>
2019-03-11 09:01:18 +0100

    Adding doc template to function invPolyArcLength3

--- src/Functions/invPolyArcLength3.m
+++ src/Functions/invPolyArcLength3.m
@@ -1,17 +1,27 @@
-%% invPolyArcLength3 - Numerically calculate the parameter (t) of parameterized [0,1] -> R^3 polynomial
-% *given* an arc length (measured from 0) i.e. find the corresponding t to an arc length
-% (e.g. given in [mm] from the origin) or in other words numerically invert polyArcLength3
-%
-% Andreas Husch
-% Centre Hospitalier de Luxembourg, Dep. of Neurosurgery /
-% University of Luxembourg - Luxembourg Centre for Systems Biomedicine
-% 2016 - 2017
-% mail@andreashusch.de, husch.andreas@chl.lu
 function t = invPolyArcLength3(polyCoeff, arcLength)
-t = zeros(size(arcLength)); % default t=0 (i.e. in caes arcLength is 0)
+% Numerically calculate the parameter (t) of parameterized [0,1] -> R^3 polynomial
+% *given* an arc length (measured from 0) i.e. find the corresponding t to an arc length
+% (e.g. given in [mm] from the origin), or in other words numerically invert polyArcLength3
+%
+% USAGE:
+%
+%    t = invPolyArcLength3(polyCoeff, arcLength)
+%
+% INPUTS: 
+%    polyCoeff:     Coefficient matrix
+%    arcLength:     
+%
+% OUTPUT: 
+%    t:     
+%
+% .. AUTHOR:
+%       - Andreas Husch, Original File
+%       - Daniel Duarte Tojal, Documentation
+
+t = zeros(size(arcLength)); % default t=0 (i.e. in case arcLength is 0)
 for i = 1:length(arcLength)
     if(arcLength(i)<0)
-        warning('invPolyArcLength3: given arcLength is negatie! Forcing t=0. This is wrong but might be approximatly okay for the use case! Check carefully!');
+        warning('invPolyArcLength3: given arcLength is negative! Forcing t=0. This is wrong but might be approximatly okay for the use case! Check carefully!');
     else
         t(i) = fminsearch( @(b)(abs(arcLength(i) - polyArcLength3(polyCoeff,0,b))), 0);
     end
