089c55631c1b3264a723aca2d9f0599f13b63a09
Loic Marx <loic.marx@gmail.com>
2019-03-20 17:28:37 +0100

    WIP apply post CT Mask

--- test/testextractElectrodePointclouds.m
+++ test/testextractElectrodePointclouds.m
@@ -14,6 +14,8 @@ fileDir = fileparts(which(mfilename));
 cd(fileDir);
 
 % load reference data
+global refDataPath
+global inputDataPath
 refData = load ([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_extractElectrodePC.mat'])
 
 % generate new data
@@ -24,5 +26,11 @@ niiCT_new = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post
 assert(isequal(elecsPointcloudStruct_new, refData.elecsPointcloudStruct_ref))
 assert(isequal(brainMask_new, refData.brainMask_ref))
 
+% apply post CT brain mask
+niiCT_brainMask_ref = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz'])
+brainMask_ref = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post_mask.nii'])
+
+brainMask_ref= [getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post_mask.nii']
+[elecsPointcloudStruct_brainMask_ref, brainMask_brainMask_ref] = extractElectrodePointclouds(niiCT_brainMask_ref,'brainMask',brainMask_ref);
 
 
