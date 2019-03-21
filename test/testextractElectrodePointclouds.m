2141569c31b0e7ef17737d1c1b050627b77ae1fa
Loic Marx <loic.marx@gmail.com>
2019-03-21 15:49:41 +0100

    compare the new data against the reference data using a BrainMask

--- test/testextractElectrodePointclouds.m
+++ test/testextractElectrodePointclouds.m
@@ -28,8 +28,12 @@ assert(isequal(elecsPointcloudStruct_new, refData.elecsPointcloudStruct_ref))
 assert(isequal(brainMask_new, refData.brainMask_ref))
 
 % apply post CT brain mask
-niiCT_brainMask_ref = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz'])
+refData_brainMask = load ([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_extractElectrodePC_withBrainMask.mat'])
+niiCT_brainMask_new = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz'])
 brainMaskPath = [getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post_mask.nii']
-[elecsPointcloudStruct_brainMask_ref, brainMask_brainMask_ref] = extractElectrodePointclouds(niiCT_brainMask_ref, 'brainMask', brainMaskPath);
+[elecsPointcloudStruct_brainMask_new, brainMask_brainMask_new] = extractElectrodePointclouds(niiCT_brainMask_new, 'brainMask', brainMaskPath);
 
+% compare the new data against the reference data using a BrainMask
+assert(isequal(elecsPointcloudStruct_brainMask_new, refData_brainMask.elecsPointcloudStruct_brainMask_ref))
+assert(isequal(brainMask_brainMask_new, refData_brainMask.brainMask_brainMask_ref))
 
