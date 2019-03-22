d57ce3767b790a96e0f67853210824b8ad2c1e7a
Loic Marx <loic.marx@gmail.com>
2019-03-22 11:06:52 +0100

    load environmental variable

--- SETUP_PACER.m
+++ SETUP_PACER.m
@@ -6,7 +6,12 @@
 % 2017
 % mail@andreashusch.de, husch.andreas@chl.lu
 
+global refDataPath
+global inputDataPath
+
 %% RUN this file once (F5) to automatically make sure all PaCER files (including subdirectories) are in your MATLAB path! 
 pacerDir = fileparts(mfilename('fullpath')); 
 addpath(genpath(pacerDir));
 savepath();
+refDataPath = [getenv('PACER_DATA_PATH') filesep 'ref' filesep];
+inputDataPath = [getenv('PACER_DATA_PATH') filesep 'input' filesep];
