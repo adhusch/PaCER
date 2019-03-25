aa8802abcb7e9dbc459ba84b5d80973e32c4c8ca
Loic Marx <loic.marx@gmail.com>
2019-03-25 15:51:34 +0100

    cleaning the code

--- src/Functions/createSimpleMPRWorldCoordinates.m
+++ src/Functions/createSimpleMPRWorldCoordinates.m
@@ -1,16 +1,15 @@
-function [mpr, oc] = createSimpleMPRWorldCoordinates(nii)
+function [mpr] = createSimpleMPRWorldCoordinates(nii)
 % Convenience Wrapper for the MPRWorldCoordinate Class by Florian Bernard
 %
 % USAGE:
 %
-%    [mpr, oc] = createSimpleMPRWorldCoordinates(nii)
+%    [mpr] = createSimpleMPRWorldCoordinates(nii)
 %
 % INPUT:
 %    nii:       NiftiModality object to visualize as multi-planar-reformatting (MPR)
 %
 % OUTPUTS:
 %    mpr:       Handle to MPR
-%    oc:        optional orientation cube (deprecated)
 %
 % .. AUTHOR:
 %       - Andreas Husch, Original file
@@ -19,6 +18,4 @@ function [mpr, oc] = createSimpleMPRWorldCoordinates(nii)
 axesObj = gca; %axesObj = axes('Parent', mprPanel); %mprPanel = uipanel('Parent', gcf);
 mpr = MPRWorldCoordinates(axesObj, nii.img, [0 0 0], nii);
 view(160,10);
-%oc = OrientationCube(mprPanel, axesObj);
-%axes(axesObj);
 end
