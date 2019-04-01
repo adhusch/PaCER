77335e2eb36f60d25e0f8879ca145d72be1d121f
Daniel Duarte Tojal <daniel.duarte.001@student.uni.lu>
2019-04-01 14:52:28 +0200

    create a copy of the source file to test on

---
+++ src/testClass/Trajectory.m
@@ -0,0 +1,88 @@
+classdef Trajectory < handle & matlab.mixin.Copyable & MetaTrajectory
+    % Trajectory - represents a (straight line) trajectory by two Point3D (entry and target) objects
+    %
+    % Parameters:
+    %
+    %    
+
+    properties
+       String = ''; %A Property of String
+    end
+
+    properties (SetObservable = true)
+        entryPoint3D = Point3D.empty; %A property of entrypoint3D
+        targetPoint3D = Point3D.empty; %A property of targetpoint3D
+    end
+    properties (Access = public, Dependent = true); 
+        entryPoint = NaN(3,1); % blabla
+        targetPoint = NaN(3,1); %hehe
+        direction = NaN(3,1); %protperty
+    end
+    
+    methods
+       function str = toString(this)
+        % Description of the function goes here
+        %
+        % Parameters:
+        %
+        %    str:   
+        %
+        % Returns:
+        %
+        %    output goes here
+        %
+            if(isempty(this.String))
+                str = ['Trajectory Object with target ' num2str(this.targetPoint')];
+            else
+                str = this.String;
+            end
+        end
+        
+        function set.entryPoint3D(this, point3DObj)
+            if(isa(point3DObj, 'Point3D'))
+                this.entryPoint3D = point3DObj;
+                this.notify('trajectoryChanged');
+            end
+        end
+        
+        function set.targetPoint3D(this, point3DObj)
+            if(isa(point3DObj, 'Point3D'))
+                this.targetPoint3D = point3DObj;
+                this.notify('trajectoryChanged');
+            end
+        end
+        
+        function value = get.entryPoint(this)
+            value = this.entryPoint3D.point;
+        end
+        
+        function value = get.targetPoint(this)
+            value = this.targetPoint3D.point;
+        end
+        
+        function set.entryPoint(this, point)
+            if(isempty(this.entryPoint3D))
+                this.entryPoint3D = Point3D();
+            end
+            this.entryPoint3D.point = point;  
+            this.notify('trajectoryChanged');
+        end
+        
+        function set.targetPoint(this, point)
+            if(isempty(this.targetPoint3D))
+                this.targetPoint3D = Point3D();
+            end
+            this.targetPoint3D.point = point;
+            this.notify('trajectoryChanged');
+        end
+        
+        function direction = get.direction(this)
+            %normalized vector from entry to destination
+            direction = -((this.entryPoint3D.point - this.targetPoint3D.point) / norm(this.entryPoint3D.point - this.targetPoint3D.point));
+        end
+    end
+end
+
+% .. AUTHORS:
+%       - Andreas Husch, Original File
+%       - Daniel Duarte Tojal, Documentation
