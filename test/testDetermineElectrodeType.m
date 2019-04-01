b494da7ee2a04bedb329fbd5729051f23efffa46
Loic Marx <loic.marx@gmail.com>
2019-04-01 09:32:40 +0200

    test for determineElectrodeType function

--- test/testDetermineElectrodeType.m
+++ test/testDetermineElectrodeType.m
@@ -6,4 +6,28 @@
 % Author:
 %     - Loic Marx, March 2019
 
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which(mfilename));
+cd(fileDir);
+
+% load reference data
+load('RefData_determineElectrodeType.mat');
+
+% define input argument
+peakDistances_new = [0.7500 1.75];
+
+% generate new output 
+[elecStruct_new, rms_new, d_new] = determineElectrodeType(peakDistances_new);
+
+% comparison between refData and new generated output
+assert(isequal(elecStruct_ref, elecStruct_new))
+assert(isequal(rms_ref, rms_new))
+assert(isequal(d_ref, d_new))
+
+% change back to the current directory
+cd(currentDir); 
+
 
