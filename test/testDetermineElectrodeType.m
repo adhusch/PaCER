0762acacf159f7a49f19c9577e550bcc74660949
Loic Marx <loic.marx@gmail.com>
2019-04-01 10:04:26 +0200

    test if the function throws a warning

--- test/testDetermineElectrodeType.m
+++ test/testDetermineElectrodeType.m
@@ -17,7 +17,7 @@ cd(fileDir);
 load('RefData_determineElectrodeType.mat');
 
 % define input argument
-peakDistances_new = [0.7500 1.75];
+peakDistances_new = [0.7500 1.75]; % electrod contact is normally 1.5mm long
 
 % generate new output 
 [elecStruct_new, rms_new, d_new] = determineElectrodeType(peakDistances_new);
@@ -27,7 +27,15 @@ assert(isequal(elecStruct_ref, elecStruct_new))
 assert(isequal(rms_ref, rms_new))
 assert(isequal(d_ref, d_new))
 
+% test if there is no objective - will throw a warning message
+w = warning ('off','all');
+peakDistances_warning = Inf;
+try
+    [elecStruct_warning, rms_warning, d_warning] = determineElectrodeType(peakDistances_warning);
+catch ME
+    assert(length(ME.message) > 0)
+end
+w = warning ('on','all');
+
 % change back to the current directory
 cd(currentDir); 
-
-
