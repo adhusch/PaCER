f4d31c83d18ad6e7e5d54ac776cc4c8afc70be73
Loic Marx <loic.marx@gmail.com>
2019-04-08 12:34:53 +0200

    WIP

--- test/suite/testPaCER.m
+++ test/suite/testPaCER.m
@@ -22,3 +22,8 @@ refData = load([refDataPath filesep 'refData_PaCER_niiCT.mat']);
 % Load post OP CT 
 niiCT_PostOP_new = NiftiMod([inputDataPath filesep 'CT_POSTOP_with_XML.nii.gz']);
 
+% generate the new output (testing only niiCT input argument)
+[elecModels_new, elecPointCloudsStruct_new, intensityProfiles_new, skelSkelmms_new] = PaCER(NiiCT_PostOp_ref);
+
+% compare the new data against the reference data
+
