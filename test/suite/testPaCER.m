c624ba1b4860ac6f37ab14fcbad17aa8c305e8ae
Loic Marx <loic.marx@gmail.com>
2019-04-09 10:53:46 +0200

    add brain mask input

--- test/suite/testPaCER.m
+++ test/suite/testPaCER.m
@@ -33,3 +33,19 @@ niiCT_PostOP_new = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep '
 assert(isequal(elecPointCloudsStruct_new, refData.elecPointCloudsStruct_ref))
 assert(isequal(intensityProfiles_new, refData.intensityProfiles_ref))
 assert(isequal(skelSkelmms_new, refData.skelSkelmms_ref))
+
+%% load reference data (provide brain mask to the CT post OP)
+refData_brainMask = load([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_PaCER_WithBrainMask.mat']);
+
+% define input arguments (testing niiCT and brainMask)
+niiCT_brainMask_new = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz']);
+brainMaskPath = [getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post_mask.nii'];
+
+% generate the new output (testing niiCT and brainMask)
+[elecModels_Mask_new, elecPointCloudsStruct_Mask_new, intensityProfiles_Mask_new, skelSkelmms_Mask_new] = PaCER(niiCT_brainMask_new,'brainMask', brainMaskPath);
+
+% compare the new data against the reference data using a BrainMask
+%assert(isequal(elecModels_Mask_new{1, 1}, refData_brainMask.elecModels_Mask_Ref{1, 1}))
+assert(isequal(elecPointCloudsStruct_Mask_new, refData_brainMask.elecPointCloudsStruct_Mask_Ref))
+assert(isequal(intensityProfiles_Mask_new, refData_brainMask.intensityProfiles_Mask_Ref))
+assert(isequal(skelSkelmms_Mask_new, refData_brainMask.skelSkelmms_Mask_Ref))
