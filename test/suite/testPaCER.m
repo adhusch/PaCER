fde5ba8ae5353fb05d3e89d23c8f6796fe2667e1
Loic Marx <loic.marx@gmail.com>
2019-04-10 16:34:32 +0200

    test different type of electrode

--- test/suite/testPaCER.m
+++ test/suite/testPaCER.m
@@ -40,7 +40,7 @@ refData_XML = load ([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_Pa
 
 % define input arguments (testing niiCT and Xml Plan)
 niiCT_Xml_new = niiCT_PostOP_new; 
-xml_Plan_new= [getenv('PACER_DATA_PATH') filesep 'input' filesep 'CT_POSTOP_with_XML.xml'];
+xml_Plan_new = [getenv('PACER_DATA_PATH') filesep 'input' filesep 'CT_POSTOP_with_XML.xml'];
 
 % generate the new output (function implemented with XML plan)
 [elecModels_XML_new, elecPointCloudsStruct_XML_new, intensityProfiles_XML_new, skelSkelmms_XML_new] = PaCER(niiCT_Xml_new,'medtronicXMLPlan', xml_Plan_new);
@@ -51,8 +51,6 @@ assert(isequal(elecPointCloudsStruct_XML_new, refData_XML.elecPointCloudsStruct_
 assert(isequal(intensityProfiles_XML_new, refData_XML.intensityProfiles_XML_ref))
 assert(isequal(skelSkelmms_XML_new, refData_XML.skelSkelmms_XML_ref))
 %
-
-
 
 %% load reference data (provide brain mask to the CT post OP)
 refData_brainMask = load([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_PaCER_WithBrainMask.mat']);
@@ -69,3 +67,41 @@ brainMaskPath = [getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post_mask
 assert(isequal(elecPointCloudsStruct_Mask_new, refData_brainMask.elecPointCloudsStruct_Mask_Ref))
 assert(isequal(intensityProfiles_Mask_new, refData_brainMask.intensityProfiles_Mask_Ref))
 assert(isequal(skelSkelmms_Mask_new, refData_brainMask.skelSkelmms_Mask_Ref))
+
+%% test different electrode type: 
+%load the reference data
+refData_electrodeType = load([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_PaCER_electrodeType.mat']);
+% define the input argument 
+niiCT_electrodesType = niiCT_PostOP_new; 
+xml_Plan_new; 
+
+%generate the new output with Medtronic 3387 electrode type. 
+[elecModels_Medtronic3387_new, elecPointCloudsStruct_Medtronic3387_new, intensityProfiles_Medtronic3387_new, skelSkelmms_Medtronic3387_new] = PaCER(niiCT_electrodesType, 'medtronicXMLPlan', xml_Plan_new, 'electrodeType', 'Medtronic 3387');
+
+%compare the new data against the reference data using XML plan and
+%providing electrode type (Medtronic 3387)
+
+%assert(isequal(elecModels_Medtronic3387_new, refData_electrodeType.elecModels_Medtronic3387_ref))
+assert(isequal(elecPointCloudsStruct_Medtronic3387_new, refData_electrodeType.elecPointCloudsStruct_Medtronic3387_ref))
+assert(isequal(intensityProfiles_Medtronic3387_new, refData_electrodeType.intensityProfiles_Medtronic3387_ref))
+assert(isequal(skelSkelmms_Medtronic3387_new, refData_electrodeType.skelSkelmms_Medtronic3387_ref))
+
+% generate the new output with Medtronic 3389 electrode type.
+[elecModels_Medtronic3389_new, elecPointCloudsStruct_Medtronic3389_new, intensityProfiles_Medtronic3389_new, skelSkelmms_Medtronic3389_new] = PaCER(niiCT_electrodesType, 'medtronicXMLPlan', xml_Plan_new, 'electrodeType', 'Medtronic 3389');
+
+%compare the new data against the reference data using XML plan and
+%providing electrode type (Medtronic 3389)
+%assert(isequal(elecModels_Medtronic3389_new, refData_electrodeType.elecModels_Medtronic3389_ref))
+assert(isequal(elecPointCloudsStruct_Medtronic3389_new, refData_electrodeType.elecPointCloudsStruct_Medtronic3389_ref))
+assert(isequal(intensityProfiles_Medtronic3389_new, refData_electrodeType.intensityProfiles_Medtronic3389_ref))
+assert(isequal(skelSkelmms_Medtronic3389_new, refData_electrodeType.skelSkelmms_Medtronic3389_ref))
+
+% generate the new output with Boston electrode type.
+[elecModels_Boston_new, elecPointCloudsStruct_Boston_new, intensityProfiles_Boston_new, skelSkelmms_Boston_new] = PaCER(niiCT_electrodesType, 'medtronicXMLPlan', xml_Plan_new, 'electrodeType', 'Boston Vercise Directional');
+
+%compare the new data against the reference data using XML plan and
+%providing electrode type (Boston Vercise Directional)
+%assert(isequal(elecModels_Boston_new, refData_electrodeType.elecModels_Boston_ref))
+assert(isequal(elecPointCloudsStruct_Boston_new, refData_electrodeType.elecPointCloudsStruct_Boston_ref))
+assert(isequal(intensityProfiles_Boston_new, refData_electrodeType.intensityProfiles_Boston_ref))
+assert(isequal(skelSkelmms_Boston_new, refData_electrodeType.skelSkelmms_Boston_ref))
