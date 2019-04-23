71125244ce457b6e7db2989e22c0b7f2b0bdb78c
Loic Marx <loic.marx@gmail.com>
2019-04-23 11:00:26 +0200

    add header to verifyFunctionWarning

---
+++ verifyFunctionWarning.m
@@ -0,0 +1,18 @@
+function success = verifyFunctionWarning(functionCall, warningMessage, varargin)
+% Tests if the warning message fits to the warning provided
+%
+% USAGE:
+%
+%    success = verifyFunctionWarning(functionCall, warningMessage, varargin)
+%
+% INPUTS:
+%    functionCall:       The name of a function.
+%    warningMessage:     The warning message that we want to capture
+%
+% OUPUTS: 
+%    success:            Catch the provided warning message
+%
+% AUTORS:
+%    Laurent Heirendt, April 2019
+%    Loic Marx, April 2019
+
