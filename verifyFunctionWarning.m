ca1c51fb4e26f9e15e6592d61250fc058ff7cd6f
Loic Marx <loic.marx@gmail.com>
2019-04-23 11:19:38 +0200

    WIP implementation of diary

--- verifyFunctionWarning.m
+++ verifyFunctionWarning.m
@@ -15,4 +15,56 @@ function success = verifyFunctionWarning(functionCall, warningMessage, varargin)
 % AUTORS:
 %    Laurent Heirendt, April 2019
 %    Loic Marx, April 2019
+%
+% NOTE: 
+%    This function has been adapted from
+%    https://github.com/opencobra/cobratoolbox/blob/master/test/verifyCobraFunctionError.m
+%    [5aa9ccd]
 
+testMessage = false;
+parser = inputParser();
+parser.addRequired('functionCall',@ischar)
+parser.addRequired('warningMessage','',@ischar)
+parser.addParamValue('inputs',{},@iscell)
+parser.addParamValue('outputArgCount',0,@(x) isnumeric(x) && mod(x,1) == 0);
+
+parser.parse(functionCall,varargin{:});
+
+outputArgcount = parser.Results.outputArgCount;
+warningMessage = ~any(ismember('warningMessage',parser.UsingDefaults));
+message = parser.Results.testMessage;
+inputs = parser.Results.inputs;
+
+functionCall = str2func(functionCall);
+success = true;
+
+verifyFunctionWarning = false;
+logFile = '.testCatch.log';
+diary(logFile); % run the function in diary mode
+try   
+    if outputArgcount > 0
+        outArgs = cell(outputArgcount,1);
+        [outArgs{:}] = functionCall(inputs{:});
+    else
+        functionCall(inputs{:});
+    end 
+end 
+
+    diary off
+    
+% loop through the temporary diary file
+    fid = fopen(logFile);
+    tline = fgetl(fid);
+    while ischar(tline)
+        tline = fgetl(fid);
+        disp(tline);
+        if ~isempty(tline) && contains(tline, warningMessage)
+            verifyFunctionWarning = true;
+            break;
+        end
+    end
+    fclose(fid);
+
+    % remove the temporary diary file
+    delete(logFile);
+end
