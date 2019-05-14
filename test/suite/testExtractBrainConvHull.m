431bff3243680f13205751ab92fb619c4838482b
Loic Marx <loic.marx@gmail.com>
2019-05-14 14:26:37 +0200

    test the warning message

--- test/suite/testExtractBrainConvHull.m
+++ test/suite/testExtractBrainConvHull.m
@@ -4,7 +4,7 @@
 %     - test the extractBrainConvHull function
 %
 % Author:
-%     - Loic Marx, March 2019
+%     - Loic Marx, May 2019
 
 global refDataPath
 global inputDataPath
@@ -21,8 +21,16 @@ cd(fileDir);
 refData = load ([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_extractBrainConvHull.mat']);
 
 % generate the new output
-niiCT= NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz']);
+niiCT = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz']);
 [convHullBrainMask_new, roughBrainMask_new] = extractBrainConvHull(niiCT);
 
 % comparison between the new output and the reference data 
-assert(isequal(convHullBrainMask, convHullBrainMask_new); 
+assert(isequal(refData.convHullBrainMask_ref, convHullBrainMask_new));
+assert(isequal(refData.roughBrainMask_ref, roughBrainMask_new)); 
+
+% test the warning message
+warningMessage = 'Uncommon fraction of CT data in threshold range (15-60 HU). Trying to compensate. Make sure to use "soft tissue" reconstruction filters for the CT (e.g. J30 kernel) if this fails.'; 
+assert(verifyFunctionWarning('extractBrainConvHull', warningMessage, 'inputs', {niiCT}));
+
+% change back to the current directory
+cd(currentDir);
