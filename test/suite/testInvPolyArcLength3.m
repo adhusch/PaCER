ad8e9c8c5e04db9e38ae81d526f97667e6e25fa8
Loic Marx <loic.marx@gmail.com>
2019-05-14 16:25:24 +0200

    WIP test the warning message

--- test/suite/testInvPolyArcLength3.m
+++ test/suite/testInvPolyArcLength3.m
@@ -13,6 +13,17 @@ currentDir = pwd;
 fileDir = fileparts(which(mfilename));
 cd(fileDir);
 
-% setup the reference data 
+%% setup the reference data to capture the warning message 
 polyCoeff = [1 2; -1 1];
-arcLength = 0; 
+arcLength = -1;  
+t_ref = 0; 
+
+% call the function 
+t_new = invPolyArcLength3(polyCoeff, arcLength);
+
+% compare the reference data and the new generated data
+assert(isequal(t_ref, t_new)); 
+
+% capture the warning message 
+warningMessage = 'invPolyArcLength3: given arcLength is negative! Forcing t=0. This is wrong but might be approximatly okay for the use case! Check carefully!';
+assert(verifyFunctionWarning('invPolyArcLength3', warningMessage, 'inputs', {polyCoeff_ref, arcLength_ref}));
