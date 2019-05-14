800eef16891ea74955448e1c2b689aef50d491f3
Loic Marx <loic.marx@gmail.com>
2019-05-14 13:46:10 +0200

    WIP load refdata

--- test/suite/testExtractBrainConvHull.m
+++ test/suite/testExtractBrainConvHull.m
@@ -17,3 +17,12 @@ currentDir = pwd;
 fileDir = fileparts(which(mfilename));
 cd(fileDir);
 
+% load reference data (function implemented with niiCT model)
+refData = load ([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_extractBrainConvHull.mat']);
+
+% generate the new output
+niiCT= NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz']);
+[convHullBrainMask_new, roughBrainMask_new] = extractBrainConvHull(niiCT);
+
+% comparison between the new output and the reference data 
+assert(isequal(convHullBrainMask, convHullBrainMask_new); 
