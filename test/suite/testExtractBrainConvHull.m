7f2241b3706b747d966b62e3c359a0de11a4de80
Loic Marx <loic.marx@gmail.com>
2019-05-14 14:36:02 +0200

    change the global setup

--- test/suite/testExtractBrainConvHull.m
+++ test/suite/testExtractBrainConvHull.m
@@ -8,7 +8,6 @@
 
 global refDataPath
 global inputDataPath
-global PACERDIR
 
 %% save the current path
 currentDir = pwd;
@@ -18,10 +17,10 @@ fileDir = fileparts(which(mfilename));
 cd(fileDir);
 
 % load reference data (function implemented with niiCT model)
-refData = load ([getenv('PACER_DATA_PATH') filesep 'ref' filesep 'refData_extractBrainConvHull.mat']);
+refData = load ([refDataPath  filesep 'refData_extractBrainConvHull.mat']);
 
 % generate the new output
-niiCT = NiftiMod([getenv('PACER_DATA_PATH') filesep 'input' filesep 'ct_post.nii.gz']);
+niiCT = NiftiMod([inputDataPath filesep 'ct_post.nii.gz']);
 [convHullBrainMask_new, roughBrainMask_new] = extractBrainConvHull(niiCT);
 
 % comparison between the new output and the reference data 
