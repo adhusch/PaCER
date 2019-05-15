94219d5d399a262cd66405e486e14989d8a91e62
Loic Marx <loic.marx@gmail.com>
2019-05-15 15:27:44 +0200

    changing input arguments value

--- test/suite/testInvPolyArcLength3.m
+++ test/suite/testInvPolyArcLength3.m
@@ -13,8 +13,8 @@ currentDir = pwd;
 fileDir = fileparts(which(mfilename));
 cd(fileDir);
 
-% setup the inputs data to capture the warning message 
-polyCoeff = [1, 2, 3]; % coefficient matrix corresponding to a 3rd degree polynomial, which maps to  3-dimensional coordinates in the last fitting step
+% setup the inputs arguments to capture the warning message 
+polyCoeff = [1 1 1; 2 3 4]; % degree 1 polynomial in 3d, i.e. a straight line through 3d space with bias (1) in each x,y,z direction and a slope of 2 in x, 3 in y and 4 and z direction.
 arcLength = -1;  
 
 % reference data hard-coded
@@ -26,22 +26,23 @@ t_new = invPolyArcLength3(polyCoeff, arcLength);
 % compare the reference data and the new generated data
 assert(isequal(t_ref, t_new)); 
 
-% capture the warning message 
+% capture the warning message when arcLength is negative
 warningMessage = 'invPolyArcLength3: given arcLength is negative! Forcing t=0. This is wrong but might be approximatly okay for the use case! Check carefully!';
 assert(verifyFunctionWarning('invPolyArcLength3', warningMessage, 'inputs', {polyCoeff, arcLength}));
 
-% setup the inputs data to test if ~arcLength(i)<0
+% setup the inputs arguments to satisfy the condition when ~arcLength(i)<0
 polyCoeff;
-arcLength = 10; 
+arcLength_positive = 10; 
+tol = 1e-4; % tolerence value for the comparison between reference data and the new output
 
 % reference data hard-coded
-t_ref = 0; 
+t_ref_positive = 5.7735; 
 
-% generate the new output 
-t_new = invPolyArcLength3(polyCoeff, arcLength);
+% generate the new output using a positive value for arcLength 
+t_new_positive = invPolyArcLength3(polyCoeff, arcLength_positive);
 
 % compare the reference data and the new generated data
-assert(isequal(t_ref, t_new));
+assert(norm(t_ref_positive - t_new_positive) < tol);
 
 % change back to the current directory
 cd(currentDir);
