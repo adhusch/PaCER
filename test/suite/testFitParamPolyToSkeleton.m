d21e1ba78513e511b821c0dc54ff52f5f6125654
Loic Marx <loic.marx@gmail.com>
2019-06-21 15:46:43 +0200

    test if degree is setup to 3

--- test/suite/testFitParamPolyToSkeleton.m
+++ test/suite/testFitParamPolyToSkeleton.m
@@ -13,8 +13,11 @@ currentDir = pwd;
 fileDir = fileparts(which(mfilename));
 cd(fileDir);
 
-% load the input arguments (function PaCER implemented only with CT_POSTOP_with_XML.nii.gz)
+% load the input arguments (obtained from function PaCER implemented only with CT_POSTOP_with_XML.nii.gz)
 fitParamPolyToSkeleton_inputs = load([inputDataPath filesep 'input_fitParamPolyToSkeleton.mat']);
+
+% setup tolerence value for comparison
+tol = 1e-6; 
 
 % load reference data 
 refData = load([refDataPath filesep 'refData_fitParamPolyToSkeleton.mat']);
@@ -25,6 +28,16 @@ refData = load([refDataPath filesep 'refData_fitParamPolyToSkeleton.mat']);
 % compare the new output with the ref data 
 assert(isequal(refData.r3polynomial, r3polynomial_new))
 assert(isequal(refData.avgTperMm, avgTperMm_new))
+
+% test if degree is setup to 3 when only the input argument skeleton is provided
+[r3polynomial_skel_new, avgTperMm_skel_new] = fitParamPolyToSkeleton(fitParamPolyToSkeleton_inputs.skeleton)
+
+% load the reference data with degree = 3
+refData_skel = load('refData_testFitParamPolyToSkeleton_skeleton.mat');
+
+% compare reference data and generated output
+assert(norm(refData_skel.r3polynomial_skel_ref - r3polynomial_skel_new)< tol)
+assert(norm(refData_skel.avgTperMm_new_skel_ref - avgTperMm_skel_new)< tol)
 
 % change back to the current directory
 cd(currentDir);
